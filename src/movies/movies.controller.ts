ebbda0d8996d8c52d0e4fd5cd98619477e297835
isrzian <shadow-sergej@ya.ru>
2022-03-04 12:56:02 +0700

    service 1 part

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -8,33 +8,38 @@ import {
   Patch,
   Delete
 } from "@nestjs/common";
+import { MoviesService } from "./movies.service";
+import { Movie } from "./entities/movie.entity";
 
 @Controller("movies")
 export class MoviesController {
 
-  @Get()
-  getAll(): string {
-    return "all films";
+  constructor(private readonly moviesService: MoviesService) {
   }
 
-  @Get("search")
-  search(@Query('year') year: string) {
-    return `I search movie realised after ${year} year`;
+  @Get()
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
   }
+
+  /*@Get("search")
+  search(@Query("year") year: string) {
+    return `I search movie realised after ${year} year`;
+  }*/
 
   @Get("/:id")
-  getOne(@Param("id") movieId: string): string {
-    return `${movieId} film`;
+  getOne(@Param("id") movieId: string): Movie {
+    return this.moviesService.getOne(movieId);
   }
 
   @Post()
-  create(@Body() movieData): string {
-    return movieData;
+  create(@Body() movieData) {
+    return this.moviesService.create(movieData);
   }
 
   @Delete("/:id")
-  remove(@Param("id") movieId: string): string {
-    return `delete film ${movieId}`;
+  remove(@Param("id") movieId: string) {
+    return this.moviesService.remove(movieId);
   }
 
   @Patch("/:id")
